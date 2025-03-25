12d49032a1660bdf3e93b6cf1f7019b80cd5303b
QI-D <qid14@outlook.com>
2025-03-25 13:56:53 -0700

    mantine setup
    Signed-off-by: QI-D <qid14@outlook.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { fetchBalance } from './redux/thunks';
+import AppHeader from './components/common/Header';
+import HomePage from './pages/HomePage';
+// import GamesPage from './pages/GamesPage';
+// import GameDetailPage from './pages/GameDetailPage';
+import { AppShell, MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+import { theme } from './theme';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const { token, isAuthenticated } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (isAuthenticated && token) {
+      dispatch(fetchBalance(token));
+    }
+  }, [dispatch, isAuthenticated, token]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+      <Notifications position="top-right" />
+      <Router>
+        <AppShell
+          header={{ height: 60 }}
+          padding="md"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <AppShell.Header>
+            <AppHeader />
+          </AppShell.Header>
+
+          <AppShell.Main>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              {/* <Route path="/games" element={<GamesPage />} />
+              <Route path="/games/:id" element={<GameDetailPage />} /> */}
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </AppShell.Main>
+        </AppShell>
+      </Router>
+    </MantineProvider>
   );
-}
+};
 
 export default App;
